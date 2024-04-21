2646623d6fda9b780aaee789e9832b8cc1fc728e
badru <mohbadrujaman9@gmail.com>
2024-04-21 20:45:27 +0700

    feat[Offline component] : Menambah fitur ketika offline

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import Arrived from "./components/Arrived";
 import Client from "./components/Client";
 import Aside from "./components/Aside";
 import Footer from "./components/Footer";
+import Offline from "./components/Offline";
 
 const App = () => {
   const [items, setItems] = useState([]);
+  const [isOnline, setIsOnline] = useState(!navigator.onLine);
 
-  console.log(items);
+  function handleOfflineStatus() {
+    setIsOnline(!navigator.onLine);
+  }
 
   const fetchItems = async () => {
     const response = await fetch(process.env.REACT_APP_APIKEY);
@@ -20,10 +24,31 @@ const App = () => {
 
   useEffect(() => {
     fetchItems();
-  }, []);
+
+    if (!document.querySelector('script[src="/carousel.js"]')) {
+      const script = document.createElement("script");
+
+      script.src = "/carousel.js";
+
+      script.async = false;
+
+      document.body.appendChild(script);
+    }
+
+    handleOfflineStatus();
+
+    window.addEventListener("online", handleOfflineStatus);
+    window.addEventListener("offline", handleOfflineStatus);
+
+    return function () {
+      window.removeEventListener("online", handleOfflineStatus);
+      window.removeEventListener("offline", handleOfflineStatus);
+    };
+  }, [isOnline]);
 
   return (
     <>
+      {isOnline && <Offline />}
       <Header />
       <Hero />
       <Browse />
---
+++ src/components/Offline.js
@@ -0,0 +1,9 @@
+export default function Offline() {
+  return (
+    <div className="flex items-center justify-center w-full min-h-screen bg-yellow-500">
+      <h1 className="text-3xl">
+        You're on offline mode, but still you can do things.{" "}
+      </h1>
+    </div>
+  );
+}
